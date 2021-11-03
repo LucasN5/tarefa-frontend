e306a22424fbdc1e27dd239988fa697e1049f53b
Figurante3 <lucasxben@hotmail.com>
2021-11-03 12:30:56 -0300

    Front-end do Cadastro de Tarefa

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+
+const Header: React.FC = () => {
+    return(
+        <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Tarefas</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Início</Nav.Link>
+              <Nav.Link href="#link">Tarefas</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> 
+    );
+}
+
+export default Header;
